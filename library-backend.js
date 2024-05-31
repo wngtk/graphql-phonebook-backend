c1e6ae69ed6448db2bb6748e2a6cab2f94d33d75
Wang Tiaoke <100271394+wngtk@users.noreply.github.com>
2024-05-31 20:55:21 +0800

    8.3: All authors

--- library-backend.js
+++ library-backend.js
@@ -105,10 +105,17 @@ const typeDefs = `
     genres: [String!]!
   }
 
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -116,7 +123,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) => {
+      return books.filter(b => b.author === root.name).length
+    }
   }
 }
 
