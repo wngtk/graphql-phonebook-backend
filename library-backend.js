6764965fce4d94deda5368d38eae4cb4bef83bb4
Wang Tiaoke <100271394+wngtk@users.noreply.github.com>
2024-05-31 21:00:55 +0800

    8.5: Books by genre

--- library-backend.js
+++ library-backend.js
@@ -114,7 +114,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -124,10 +124,14 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
-        return books
+      let res = books
+      if (args.author) {
+        res = res.filter(b => b.author === args.author)
       }
-      return books.filter(b => b.author === args.author)
+      if (args.genre) {
+        res = res.filter(b => b.genres.includes(args.genre))
+      }
+      return res
     },
     allAuthors: () => authors
   },
