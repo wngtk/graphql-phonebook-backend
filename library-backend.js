75cb201a8f3907ceab7f5ca65ce7f776e1b9fc4d
Wang Tiaoke <100271394+wngtk@users.noreply.github.com>
2024-05-31 20:51:00 +0800

    8.2: All books

--- library-backend.js
+++ library-backend.js
@@ -98,16 +98,25 @@ let books = [
 */
 
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
